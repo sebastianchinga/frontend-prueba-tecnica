6a1fc0d65f84b1810fbc1d1f47a8cd592803dfc3
Sebastian <jchingapalacios05@gmail.com>
2025-06-25 23:31:26 -0500

    Agregando variable de entorno de vite

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: import.meta.env.VITE_URL_BACKEND
 })
 
 export default clienteAxios
