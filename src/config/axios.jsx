1b156a6ab5e080aaca41720b93fb31521953f2f2
Sebastián Chinga <60305989+sebastianchinga@users.noreply.github.com>
2025-07-04 15:54:14 -0500

    Modificando URL del backend

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'https://backend-prueba-tecnica-280s.onrender.com'
+    baseURL: 'https://backend-prueba-tecnica-280s.onrender.com/api'
 })
 
 export default clienteAxios
